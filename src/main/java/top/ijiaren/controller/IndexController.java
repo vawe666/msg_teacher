be9d4556d644d1d0cb107cc11707af40a76f7d37
zhengbotao <zhengbotao@autohome.com.cn>
2020-07-22 19:31:16 +0800

    添加模板---更正

--- src/main/java/top/ijiaren/controller/IndexController.java
+++ src/main/java/top/ijiaren/controller/IndexController.java
@@ -1,10 +1,12 @@
 package top.ijiaren.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+@Controller
+@RequestMapping("test")
 public class IndexController {
-    @RequestMapping
+    @RequestMapping("index")
     public String Index() {
         return "index";
     }
